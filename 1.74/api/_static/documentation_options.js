5649a1edf060170d09e06694a9b49f7f6442fa6e
Markus Piotrowski <Markus.Piotrowski@ruhr-uni-bochum.de>
2020-09-18 08:43:43 +0200

    Rebuilt docs for Biopython 1.74

--- 1.74/api/_static/documentation_options.js
+++ 1.74/api/_static/documentation_options.js
@@ -3,7 +3,9 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '1.74',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
